75a579fbe93b0db06db57336f7b6003ffb08af94
LintonRobinson <40444726+LintonRobinson@users.noreply.github.com>
2026-03-06 09:56:45 -0500

    feat: add useFetchCart hook to fetch a users cart by id

---
+++ src/hooks/useFetchCart.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+function useFetchCart({ userId }) {
+  const [userCartData, setUserCartData] = useState(null);
+  const [loadingState, setLoadingState] = useState(true);
+  const [error, setError] = useState(null);
+  const fetchUserCart = async () => {
+    try {
+      const response = await fetch(`https://fakestoreapi.com/carts/${userId}`);
+      if (!response.ok) {
+        throw Error("Network response was not ok");
+      }
+      const fetchedCartData = await response.json();
+      setUserCartData(fetchedCartData);
+      setLoadingState(false);
+    } catch (err) {
+      setError(err);
+      setLoadingState(false);
+    } finally {
+      setLoadingState(false);
+    }
+  };
+  fetchUserCart();
+  return { loadingState, userCartData, error };
+}
+
+export default useFetchCart;
