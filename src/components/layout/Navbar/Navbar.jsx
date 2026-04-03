51edcc69088ba548bea3bf35fbc1ee393e11dd21
LintonRobinson <40444726+LintonRobinson@users.noreply.github.com>
2026-04-03 17:37:11 -0400

    `refactor(Navbar): wrap nav in semantic header element`

--- src/components/layout/Navbar/Navbar.jsx
+++ src/components/layout/Navbar/Navbar.jsx
@@ -6,30 +6,32 @@ import { LuShoppingBag } from "react-icons/lu";
 
 function Navbar({ userCart }) {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/" aria-label="Home Page" className={styles.logoContainer}>
-            <img src={logo} alt="" />
-          </Link>
-        </li>
-        <li>
-          <NavLink to="/" aria-label="Home Page" className={({ isActive }) => (isActive ? `${styles.navLink} ${styles.active}` : styles.navLink)}>
-            Home
-          </NavLink>
-          <NavLink to="/shop" aria-label="Shop All Page" className={({ isActive }) => (isActive ? `${styles.navLink} ${styles.active}` : styles.navLink)}>
-            Shop
-          </NavLink>
-        </li>
-        <li className={styles.navbarRight}>
-          <ProductSearchBar />
-          <Link key={userCart} to="/cart" className={styles.shoppingBag} aria-label="Go To User Cart">
-            <LuShoppingBag size={22} />
-            {userCart > 0 && <span className={styles.shoppingBagNumber}>{userCart}</span>}
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/" aria-label="Home Page" className={styles.logoContainer}>
+              <img src={logo} alt="" />
+            </Link>
+          </li>
+          <li>
+            <NavLink to="/" aria-label="Home Page" className={({ isActive }) => (isActive ? `${styles.navLink} ${styles.active}` : styles.navLink)}>
+              Home
+            </NavLink>
+            <NavLink to="/shop" aria-label="Shop All Page" className={({ isActive }) => (isActive ? `${styles.navLink} ${styles.active}` : styles.navLink)}>
+              Shop
+            </NavLink>
+          </li>
+          <li className={styles.navbarRight}>
+            <ProductSearchBar />
+            <Link key={userCart} to="/cart" className={styles.shoppingBag} aria-label="Go To User Cart">
+              <LuShoppingBag size={22} />
+              {userCart > 0 && <span className={styles.shoppingBagNumber}>{userCart}</span>}
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
 
